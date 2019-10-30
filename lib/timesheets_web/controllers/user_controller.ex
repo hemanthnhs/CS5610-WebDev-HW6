b9a542bb4c18a406e15cbc3bfa36c5c026821e6c
Hemanth <hemanthsai10@gmail.com>
2019-10-30 13:11:34 -0400

    Password implementation

--- lib/timesheets_web/controllers/user_controller.ex
+++ lib/timesheets_web/controllers/user_controller.ex
@@ -1,28 +1,42 @@
-defmodule TimesheetsWeb.SessionController do
+defmodule TimesheetsWeb.UserController do
   use TimesheetsWeb, :controller
 
-  def new(conn, _params) do
-    render(conn, "new.html")
+  alias Timesheets.Users
+  alias Timesheets.Users.User
+
+  def index(conn, _params) do
+    users = Users.list_users()
+    render(conn, "index.html", users: users)
   end
 
-  def create(conn, %{"email" => email}) do
-    user = Timesheets.Users.get_user_by_email(email)
-    if user do
-      conn
-      |> put_session(:user_id, user.id)
-      |> put_flash(:info, "Welcome back #{user.email}")
-      |> redirect(to: Routes.page_path(conn, :index))
-    else
-      conn
-      |> put_flash(:error, "Login failed.")
-      |> redirect(to: Routes.page_path(conn, :index))
+  def new(conn, _params) do
+    changeset = Users.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Users.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def delete(conn, _params) do
+  def show(conn, %{"id" => id}) do
+    user = Users.get_user_with_photos!(id)
+    render(conn, "show.html", user: user)
+  end
+
+  def delete(conn, %{"id" => id}) do
+    user = Users.get_user!(id)
+    {:ok, _user} = Users.delete_user(user)
+
     conn
-    |> delete_session(:user_id)
-    |> put_flash(:info, "Logged out.")
-    |> redirect(to: Routes.page_path(conn, :index))
+    |> put_flash(:info, "User deleted successfully.")
+    |> redirect(to: Routes.user_path(conn, :index))
   end
 end
